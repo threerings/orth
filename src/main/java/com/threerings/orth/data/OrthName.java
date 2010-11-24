d255c4e76085606e4ff5c352fa41cf075f8f5c3e
Par Winzell <par.winzell@threerings.net>
2010-11-24 09:43:45 -0600

    Break out the idea of a Name with a unique identifier, something both Whirled and Doctor will depend on.

---
+++ src/main/java/com/threerings/orth/data/OrthName.java
@@ -0,0 +1,92 @@
+//
+// $Id: $
+
+
+package com.threerings.orth.data;
+
+import java.util.Comparator;
+
+import com.google.common.primitives.Ints;
+
+import com.threerings.presents.dobj.DSet;
+import com.threerings.util.Name;
+
+/**
+ * Extends {link Name} with a unique identifier useful for persistence.
+ *
+ * <p> GWT NOTE: this class (and all {@link Name} derivatives} must use custom field serializers
+ * because IsSerializable only serializes the fields in the class that declares that interface and
+ * all subclasses, it does not serialize fields from the superclass. In this case, we have fields
+ * from our superclass that need to be serialized, but we can't make {@link Name} implement
+ * IsSerializable without introducing an otherwise unwanted dependency on GWT in Narya.
+ *
+ * <p> If you extend this class (or if you extend {@link Name}) you will have to implement a custom
+ * field serializer for your derived class.
+ */
+public class OrthName extends Name implements DSet.Entry
+{
+    /** A comparator for sorting Names by their display portion, case insensitively. */
+    public static final Comparator<OrthName> BY_DISPLAY_NAME = new Comparator<OrthName>() {
+        public int compare (OrthName name1, OrthName name2) {
+            return compareNames(name1, name2);
+        }
+    };
+
+    /**
+     * Compares two member name records case insensitively.
+     */
+    public static int compareNames (OrthName m1, OrthName m2)
+    {
+        return m1.toString().toLowerCase().compareTo(m2.toString().toLowerCase());
+    }
+
+    /** For unserialization. */
+    public OrthName ()
+    {
+    }
+
+    /** Create a new Orthname with the given display name and id. */
+    public OrthName (String name, int id)
+    {
+        super(name);
+
+        _id = id;
+    }
+
+    /**
+     * Return the id of this user.
+     */
+    public int getId ()
+    {
+        return _id;
+    }
+
+    public Comparable<?> getKey ()
+    {
+        return _id;
+    }
+
+    @Override // from Name
+    public int hashCode ()
+    {
+        return _id;
+    }
+
+    @Override // from Name
+    public boolean equals (Object other)
+    {
+        return (other instanceof OrthName) && (((OrthName) other).getId() == _id);
+    }
+
+    @Override // from Name
+    public int compareTo (Name o)
+    {
+        // Note: You may be tempted to have names sort by the String value, but Names are used
+        // as DSet keys in various places and so each user's must be unique.
+        // Use BY_DISPLAY_NAME to sort names for display.
+        return Ints.compare(_id, ((OrthName) o).getId());
+    }
+
+    /** The member id of the member we represent. */
+    protected int _id;
+}
---
+++ src/main/java/com/threerings/orth/data/OrthName_CustomFieldSerializer.java
@@ -0,0 +1,31 @@
+//
+// $Id: OrthName_CustomFieldSerializer.java 9944 2008-07-22 17:30:34Z mdb $
+
+package com.threerings.orth.data;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.SerializationStreamReader;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+
+/**
+ * Custom field serializer for {@link OrthName}.
+ */
+public final class OrthName_CustomFieldSerializer
+{
+    public static void serialize (SerializationStreamWriter streamWriter, OrthName name)
+        throws SerializationException
+    {
+        streamWriter.writeString(name.toString());
+        streamWriter.writeInt(name.getId());
+    }
+
+    public static OrthName instantiate (SerializationStreamReader streamReader)
+        throws SerializationException
+    {
+        return new OrthName(streamReader.readString(), streamReader.readInt());
+    }
+
+    public static void deserialize (SerializationStreamReader streamReader, OrthName instance)
+    {
+    }
+}
