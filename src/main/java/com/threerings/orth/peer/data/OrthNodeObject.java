d238978de232433e6846024989bbffe717cb0218
Par Winzell <par.winzell@threerings.net>
2010-12-15 11:07:43 -0600

    Don't make an ActionScript variant of OrthNodeObject.

--- src/main/java/com/threerings/orth/peer/data/OrthNodeObject.java
+++ src/main/java/com/threerings/orth/peer/data/OrthNodeObject.java
@@ -1,14 +1,16 @@
 //
 // $Id: $
 
-
 package com.threerings.orth.peer.data;
+
+import com.threerings.util.ActionScript;
 
 import com.threerings.crowd.peer.data.CrowdNodeObject;
 
 /**
  *
  */
+@ActionScript(omit=true)
 public class OrthNodeObject extends CrowdNodeObject
 {
 }
