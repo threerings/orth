400038d5541d9f68f9d5db6502c3b9ce137c8fa6
Par Winzell <par.winzell@threerings.net>
2010-12-20 18:25:02 -0600

    We're going to try to get away with OrthPeerManager deriving from PeerManager, skipping CrowdPeerManager. The crowd peer support seems entirely focused on chatting, and Orth does not support crowd chatting. If we undo this decision, we will find some other way to deal with the fact that CrowdPeerManager.initClientInfo() assumes there is always a body, and consequently that there is a visibleName. This is just one of the many places where crowd chatting still interfaces badly with the notion of bodyless connections.

--- src/main/java/com/threerings/orth/peer/server/OrthPeerManager.java
+++ src/main/java/com/threerings/orth/peer/server/OrthPeerManager.java
@@ -11,10 +11,9 @@ import com.google.common.collect.Maps;
 import com.samskivert.util.Lifecycle;
 import com.samskivert.util.ObserverList;
 
-import com.threerings.crowd.peer.server.CrowdPeerManager;
-
 import com.threerings.presents.peer.data.ClientInfo;
 import com.threerings.presents.peer.data.NodeObject;
+import com.threerings.presents.peer.server.PeerManager;
 import com.threerings.presents.peer.server.PeerNode;
 import com.threerings.presents.server.PresentsSession;
 
@@ -26,7 +25,7 @@ import com.threerings.orth.peer.data.OrthNodeObject;
 /**
  *
  */
-public abstract class OrthPeerManager extends CrowdPeerManager
+public abstract class OrthPeerManager extends PeerManager
 {
     /** Our {@link PeerObserver}s. */
     public final ObserverList<PeerObserver> peerObs = ObserverList.newFastUnsafe();
@@ -46,13 +45,20 @@ public abstract class OrthPeerManager extends CrowdPeerManager
         super(cycle);
     }
 
-    @Override // from CrowdPeerManager
+    @Override // from PeerManager
     protected NodeObject createNodeObject ()
     {
         return (_onobj = new OrthNodeObject());
     }
 
-    @Override // from CrowdPeerManager
+
+    @Override // from PeerManager
+    protected ClientInfo createClientInfo ()
+    {
+        return new OrthClientInfo();
+    }
+
+    @Override // from PeerManager
     protected void initClientInfo (PresentsSession client, ClientInfo info)
     {
         super.initClientInfo(client, info);
