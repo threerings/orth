4da019f7b658f6b64ed239832c77eb0e5ad8f5cd
Charlie Groves <charlie.groves@gmail.com>
2011-01-05 12:40:55 -0800

    Rerun genservice to get rid of some deprecation warnings

--- src/main/java/com/threerings/orth/room/client/PetService.java
+++ src/main/java/com/threerings/orth/room/client/PetService.java
@@ -3,7 +3,6 @@
 
 package com.threerings.orth.room.client;
 
-import com.threerings.presents.client.Client;
 import com.threerings.presents.client.InvocationService;
 
 /**
@@ -16,17 +15,16 @@ public interface PetService extends InvocationService
      * room occupied by the caller. If the pet is loaded, it will simply move to the room occupied
      * by the caller. In both cases, the pet will automatically be placed in follow mode.
      */
-    void callPet (Client client, int petId, ConfirmListener listener);
+    void callPet (int petId, ConfirmListener listener);
 
     /**
      * Issues an order to the specified pet. Different orders have different access control
      * restrictions, see the (TODO) Pet order constants for details.
      */
-    void orderPet (Client client, int petId, int order, ConfirmListener listener);
+    void orderPet (int petId, int order, ConfirmListener listener);
 
     /**
      * Sends a chat message from the pet with specified id and name.
      */
-    public void sendChat (
-        Client client, int bodyOid, int sceneId, String message, ConfirmListener listener);
+    public void sendChat (int bodyOid, int sceneId, String message, ConfirmListener listener);
 }
