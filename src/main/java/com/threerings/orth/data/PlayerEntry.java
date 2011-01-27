4d182ee6e3669b7f9c9b061cdb1a13f6cbd84198
Par Winzell <par.winzell@threerings.net>
2011-01-27 10:39:34 -0600

    Whenever we need to store/reference another player by name/id/photo. This is the base class for FriendEntry and PartyPeep also.

---
+++ src/main/java/com/threerings/orth/data/PlayerEntry.java
@@ -0,0 +1,50 @@
+//
+// $Id: PlayerEntry.java 19625 2010-11-24 15:47:54Z zell $
+
+package com.threerings.orth.data;
+
+import com.threerings.presents.dobj.DSet;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class PlayerEntry
+    implements DSet.Entry
+{
+    /** The display name of the friend. */
+    public VizOrthName name;
+
+    /** Suitable for deserialization. */
+    public PlayerEntry ()
+    {
+    }
+
+    public PlayerEntry (VizOrthName name)
+    {
+        this.name = name;
+    }
+
+    // from interface DSet.Entry
+    public Comparable<?> getKey ()
+    {
+        return this.name.getKey();
+    }
+
+    @Override // from Object
+    public int hashCode ()
+    {
+        return this.name.hashCode();
+    }
+
+    @Override // from Object
+    public boolean equals (Object other)
+    {
+        return (other instanceof PlayerEntry) &&
+            (this.name.getId() == ((PlayerEntry)other).name.getId());
+    }
+
+    @Override
+    public String toString ()
+    {
+        return "PlayerEntry[" + name + "]";
+    }
+}
