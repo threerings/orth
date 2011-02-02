a49f95abfbb67b3f0b65067accc5650fa54b0dfe
Par Winzell <par.winzell@threerings.net>
2011-02-02 12:04:44 -0600

    I am not sure WorldService/WorldDirector and friends shouldn't move elsewhere, but until they do, we need WorldCodes, too.

---
+++ src/main/java/com/threerings/orth/world/data/WorldCodes.java
@@ -0,0 +1,12 @@
+//
+// $Id: $
+
+package com.threerings.world.data;
+
+import com.threerings.presents.data.InvocationCodes;
+
+public class WorldCodes extends InvocationCodes
+{
+    /** A message even dispatched on the member object to followers. */
+    public static final String FOLLOWEE_MOVED = "folMov";
+}
