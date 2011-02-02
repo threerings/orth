b2a687f5c9adec5dfa972df1a4c53745089fad36
Par Winzell <par.winzell@threerings.net>
2011-02-02 12:05:51 -0600

    Err. This was meant to be an interface.

--- src/main/java/com/threerings/orth/world/data/WorldCodes.java
+++ src/main/java/com/threerings/orth/world/data/WorldCodes.java
@@ -5,7 +5,7 @@ package com.threerings.world.data;
 
 import com.threerings.presents.data.InvocationCodes;
 
-public class WorldCodes extends InvocationCodes
+public interface WorldCodes extends InvocationCodes
 {
     /** A message even dispatched on the member object to followers. */
     public static final String FOLLOWEE_MOVED = "folMov";
