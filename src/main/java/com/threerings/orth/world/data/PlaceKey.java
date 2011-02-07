6682fdf0f924c843cd0d18661b4a7b068b073d19
Par Winzell <par.winzell@threerings.net>
2011-02-07 14:28:42 -0600

    PlaceKey becomes an abstract class; RoomKey extends it. The Java version goes to some trouble to make sure compareTo() orders by first type (it will be used as a DSet.Entry key). This data object will no longer be responsible for the rather onerous task of creating OrthPlaces.

--- src/main/java/com/threerings/orth/world/data/PlaceKey.java
+++ src/main/java/com/threerings/orth/world/data/PlaceKey.java
@@ -3,17 +3,24 @@
 
 package com.threerings.orth.world.data;
 
-import com.threerings.io.Streamable;
+import com.threerings.io.SimpleStreamableObject;
 
 /**
  * The base type for a specification of a place, used both to instruct the Orth system to
  * host/instantiate the place and for a peer to announce it's hosting it.
  */
-public interface PlaceKey extends Comparable<PlaceKey>, Streamable
+public abstract class PlaceKey extends SimpleStreamableObject
+    implements Comparable<PlaceKey>
 {
     /** A short, opaque string uniquely identifying what type of place this is. */
-    public String getPlaceType ();
+    public abstract String getPlaceType ();
 
-    /** Construct a type-appropriate {@link OrthPlace} object, given a hosting peer. */
-    public OrthPlace toPlace (String hostingPeer);
+    @Override final public int compareTo (PlaceKey other)
+    {
+        int byType = getPlaceType().compareTo(other.getPlaceType());
+        return (byType != 0) ? byType : compareWithinType(other);
+    }
+
+    /** Compare this object to another, with the knowledge that it's of the same type. */
+    protected abstract int compareWithinType (PlaceKey other);
 }
