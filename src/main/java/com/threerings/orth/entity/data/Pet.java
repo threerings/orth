0a6cc45d521f8be75059d0350ee701efa3fde8f4
Par Winzell <par.winzell@threerings.net>
2011-02-07 21:46:30 -0600

    New Entity type for Pets.

---
+++ src/main/java/com/threerings/orth/entity/data/Pet.java
@@ -0,0 +1,23 @@
+//
+// $Id: $
+
+package com.threerings.orth.entity.data;
+
+import com.threerings.orth.data.MediaDesc;
+
+/**
+ * Client-side information about an entity that can represent a player (or perhaps an NPC).
+ */
+public interface Pet
+    extends Entity
+{
+    /**
+     * Returns a media descriptor for the media that implements our pet.
+     */
+    MediaDesc getPetMedia ();
+
+    /**
+     * Return the scaling to apply to the pet media.
+     */
+    float getScale ();
+}
