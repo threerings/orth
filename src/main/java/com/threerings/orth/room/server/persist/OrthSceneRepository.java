36dc27deb956b36950ed34e8a951d1f5e178360c
Par Winzell <par.winzell@threerings.net>
2011-02-14 17:44:16 -0600

    I had actually gone and implemented the client SceneRepository, brainlessly. Sigh. Here's the real one.

--- src/main/java/com/threerings/orth/room/server/persist/OrthSceneRepository.java
+++ src/main/java/com/threerings/orth/room/server/persist/OrthSceneRepository.java
@@ -7,19 +7,22 @@ import java.io.IOException;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.samskivert.io.PersistenceException;
 
 import com.threerings.orth.entity.data.Decor;
 import com.threerings.orth.room.data.OrthSceneModel;
 import com.threerings.whirled.server.persist.SceneRepository;
 import com.threerings.whirled.util.NoSuchSceneException;
+import com.threerings.whirled.util.UpdateList;
 import com.threerings.whirled.data.SceneModel;
+import com.threerings.whirled.data.SceneUpdate;
 
 @Singleton
 public class OrthSceneRepository
     implements SceneRepository
 {
     public SceneModel loadSceneModel (int sceneId)
-        throws IOException, NoSuchSceneException
+        throws PersistenceException, NoSuchSceneException
     {
         if (sceneId != 1) {
             throw new IllegalArgumentException("Only scene 1 has been implemented!");
@@ -27,14 +30,20 @@ public class OrthSceneRepository
         return OrthSceneModel.blankOrthSceneModel();
     }
 
-    public void storeSceneModel (SceneModel model)
-        throws IOException
+    public UpdateList loadUpdates (int sceneId)
+        throws PersistenceException
     {
-        throw new IllegalStateException("not implemented");
+        return new UpdateList();
     }
 
-    public void deleteSceneModel (int sceneId)
-        throws IOException
+    public Object loadExtras (int sceneId, SceneModel model)
+        throws PersistenceException
+    {
+        return null;
+    }
+
+    public void applyAndRecordUpdate (SceneModel model, SceneUpdate update)
+        throws PersistenceException
     {
         throw new IllegalStateException("not implemented");
     }
