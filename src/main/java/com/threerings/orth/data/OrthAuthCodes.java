b82d4647afb88b44ceba4740b447740411289ce6
Par Winzell <par.winzell@threerings.net>
2011-02-17 00:49:46 -0600

    We need these in the Orth layer.

---
+++ src/main/java/com/threerings/orth/data/OrthAuthCodes.java
@@ -0,0 +1,24 @@
+//
+// $Id: $
+
+package com.threerings.orth.data;
+
+import com.threerings.presents.data.AuthCodes;
+import com.threerings.util.ActionScript;
+
+/**
+ * Additional authorization codes for Orth.
+ */
+@ActionScript(omit=true)
+public interface OrthAuthCodes extends AuthCodes
+{
+    /** A code indicating that the client version is out of date. */
+    public static final String VERSION_MISMATCH = "m.version_mismatch";
+
+    /** A code indicating that the client has a newer version of the code
+     * than the server which generally means we're in the middle of updating the game. */
+    public static final String NEWER_VERSION = "m.newer_version";
+
+    /** A code indicating that a client's session has expired. */
+    public static final String SESSION_EXPIRED = "m.session_expired";
+}
