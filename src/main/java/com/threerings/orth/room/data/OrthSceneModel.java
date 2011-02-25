f0f43fda089e25db2584d7b81f1d40e134ad3382
Par Winzell <par.winzell@threerings.net>
2011-02-25 15:02:12 -0600

    Imports.

--- src/main/java/com/threerings/orth/room/data/OrthSceneModel.java
+++ src/main/java/com/threerings/orth/room/data/OrthSceneModel.java
@@ -11,11 +11,8 @@ import com.threerings.util.Name;
 import com.threerings.whirled.data.SceneModel;
 import com.threerings.whirled.spot.data.Portal;
 
-import com.threerings.orth.data.MediaDesc;
-import com.threerings.orth.data.MediaMimeTypes;
 import com.threerings.orth.data.URLMediaDesc;
 
-import com.threerings.orth.entity.data.Decor;
 import com.threerings.orth.entity.data.DecorData;
 
 import static com.threerings.orth.Log.log;
