ec222caa72a9f3900dd53a65cb08c9b6a75b927b
Jamie Doornbos <jamie@threerings.net>
2011-03-30 15:33:01 -0700

    Client resolver for aether, pulling in some who functionality and streamlining for subclassing

---
+++ src/main/java/com/threerings/orth/aether/server/AetherClientResolver.java
@@ -0,0 +1,59 @@
+package com.threerings.orth.aether.server;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
+
+import com.threerings.crowd.server.CrowdClientResolver;
+import com.threerings.orth.aether.data.PlayerObject;
+import com.threerings.orth.aether.server.persist.RelationshipRepository;
+import com.threerings.orth.data.AuthName;
+import com.threerings.orth.peer.server.OrthPeerManager;
+import com.threerings.orth.server.persist.OrthPlayerRecord;
+import com.threerings.orth.server.persist.OrthPlayerRepository;
+import com.threerings.presents.data.ClientObject;
+
+/**
+ * Performs resolution of aether clients.
+ */
+public class AetherClientResolver extends CrowdClientResolver
+{
+    @Override // from ClientResolver
+    public PlayerObject createClientObject ()
+    {
+        return new PlayerObject();
+    }
+
+    @Override // from ClientResolver
+    public PlayerLocal createLocalAttribute ()
+    {
+        return new PlayerLocal();
+    }
+
+    @Override // from ClientResolver
+    protected void resolveClientData (ClientObject clobj)
+        throws Exception
+    {
+        super.resolveClientData(clobj);
+        clobj.getLocal(PlayerLocal.class).init();
+        resolvePlayer((PlayerObject)clobj);
+    }
+
+    /**
+     * Resolves the members for an aether player. This is called on the invoker thread.
+     */
+    protected void resolvePlayer (final PlayerObject plobj)
+    {
+        int playerId = ((AuthName)_username).getId();
+
+        // load up their player information using on their authentication (account) name
+        _playerRec = Preconditions.checkNotNull(_playerRepo.loadPlayer(playerId),
+            "Missing player record for authenticated player? [username=%s]", _username);
+
+        // TODO: setup plobj.friends
+    }
+
+    protected OrthPlayerRecord _playerRec;
+
+    // dependencies
+    @Inject protected OrthPlayerRepository _playerRepo;
+}
