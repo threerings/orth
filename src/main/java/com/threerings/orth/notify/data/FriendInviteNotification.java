b6e3a692511bc54901662bad8f957cc5a611494c
Jamie Doornbos <jamie@threerings.net>
2011-03-29 20:45:30 -0700

    A notification for friend invites.

---
+++ src/main/java/com/threerings/orth/notify/data/FriendInviteNotification.java
@@ -0,0 +1,28 @@
+package com.threerings.orth.notify.data;
+
+import com.threerings.orth.aether.data.PlayerName;
+
+/**
+ * Notification sent when friendship is requested.
+ */
+public class FriendInviteNotification extends Notification
+{
+    /** Creates a new notification for the given sender. */
+    public FriendInviteNotification(PlayerName sender)
+    {
+        _sender = sender;
+    }
+
+    /** For deserialization */
+    public FriendInviteNotification() {}
+
+    @Override
+    public String getAnnouncement ()
+    {
+        // implemented on client
+        return null;
+    }
+
+    /** The player that wants to be friends. */
+    protected PlayerName _sender;
+}
