b9e616f49f1be78378beceec8c0c78bce461dc7c
Jamie Doornbos <jamie@threerings.net>
2011-04-01 15:53:36 -0700

    Comments

--- src/main/java/com/threerings/orth/aether/server/persist/RelationshipRepository.java
+++ src/main/java/com/threerings/orth/aether/server/persist/RelationshipRepository.java
@@ -11,7 +11,8 @@ import java.util.Collection;
 public interface RelationshipRepository
 {
     /**
-     * Adds a symmetric friend relationship between the two players with the given ids.
+     * Adds a symmetric friend relationship between the two players with the given ids. If the
+     * friendship is already in place, does nothing.
      */
     public void addFriendship (int playerId1, int playerId2);
 
@@ -22,7 +23,8 @@ public interface RelationshipRepository
     public boolean removeFriendship (int playerId1, int playerId2);
 
     /**
-     * Retrieves all the player ids of the friends of the given player.
+     * Retrieves all the player ids of the friends of the given player. Note that no uniqueness
+     * guarantee is made. Some ids may appear twice.
      */
     public Collection<Integer> getFriendIds (int playerId);
 }
