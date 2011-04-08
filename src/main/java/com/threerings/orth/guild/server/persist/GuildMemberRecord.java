4c745f30401a7453b43640ca9dce3e22e16b8d60
Jamie Doornbos <jamie@threerings.net>
2011-04-08 15:27:32 -0700

    Comments

--- src/main/java/com/threerings/orth/guild/server/persist/GuildMemberRecord.java
+++ src/main/java/com/threerings/orth/guild/server/persist/GuildMemberRecord.java
@@ -7,9 +7,23 @@ import java.util.Date;
 
 import com.threerings.orth.guild.data.GuildRank;
 
+/**
+ * Abstraction of persistent guild membership data for orth.
+ */
 public interface GuildMemberRecord
 {
+    /**
+     * Gets the id of the player that belongs to the guild.
+     */
     int getPlayerId ();
+
+    /**
+     * Gets the date the player joined the guild.
+     */
     Date getDateJoined ();
+
+    /**
+     * Gets the rank of the player in the guild.
+     */
     GuildRank getRank ();
 }
--- src/main/java/com/threerings/orth/guild/server/persist/GuildRecord.java
+++ src/main/java/com/threerings/orth/guild/server/persist/GuildRecord.java
@@ -1,7 +1,17 @@
 package com.threerings.orth.guild.server.persist;
 
+/**
+ * Abstraction of persistent guild data for orth.
+ */
 public interface GuildRecord
 {
+    /**
+     * Gets the unique id of the guild.
+     */
     int getGuildId ();
+
+    /**
+     * Gets the name of the guild.
+     */
     String getName ();
 }
