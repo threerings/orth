122882dd05a325d71ca99b3732994555acb04397
Jamie Doornbos <jamie@threerings.net>
2011-04-13 15:20:38 -0700

    Extend invocation codes so we get all the usual error strings.

--- src/main/java/com/threerings/orth/guild/data/GuildCodes.java
+++ src/main/java/com/threerings/orth/guild/data/GuildCodes.java
@@ -1,9 +1,11 @@
 package com.threerings.orth.guild.data;
 
+import com.threerings.presents.data.InvocationCodes;
+
 /**
  * UI identifiers relating to use of the guild service or guild methods in the player service.
  */
-public interface GuildCodes
+public interface GuildCodes extends InvocationCodes
 {
     /** Error thrown when a player is already in a guild. */
     public static final String E_PLAYER_ALREADY_IN_GUILD = "e.player_already_in_guild";
