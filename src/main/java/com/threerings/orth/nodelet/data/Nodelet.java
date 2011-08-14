61dfc5a441d31a980752d3811039d00c3e9d293f
Pär Winzell <zell@threerings.net>
2011-08-14 00:19:57 -0500

    This looks like something that was since removed.

--- src/main/java/com/threerings/orth/nodelet/data/Nodelet.java
+++ src/main/java/com/threerings/orth/nodelet/data/Nodelet.java
@@ -9,9 +9,7 @@ import com.threerings.io.SimpleStreamableObject;
 /**
  * The base type for an object that is subscribed to simultaneously by multiple players and is
  * hosted on a single server, which each subscriber must connect to. The nodelet is used to
- * instantiate the shared {@code DObject} that players will subscribe to. If the {@link
- * #Publishable} interface is also implemented, then the nodelet may also be used to publish
- * the hosting state to other peers.
+ * instantiate the shared {@code DObject} that players will subscribe to.
  */
 public abstract class Nodelet extends SimpleStreamableObject
 {
