8fef9786f8473873517738162dc2a75d1b0143fb
Pär Winzell <zell@threerings.net>
2011-09-15 14:37:03 -0500

    This is pointless. But we're keeping the class itself for easy subclassing.

--- src/main/java/com/threerings/orth/room/server/RoomSession.java
+++ src/main/java/com/threerings/orth/room/server/RoomSession.java
@@ -4,20 +4,8 @@
 
 package com.threerings.orth.room.server;
 
-import com.threerings.presents.net.BootstrapData;
 import com.threerings.presents.server.PresentsSession;
 
 public class RoomSession extends PresentsSession
 {
-    @Override
-    protected void sessionConnectionClosed ()
-    {
-        super.sessionConnectionClosed();
-    }
-
-    @Override
-    protected void populateBootstrapData (BootstrapData data)
-    {
-        super.populateBootstrapData(data);
-    }
 }
