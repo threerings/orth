3f0226042f2d0bd733a6cdffcf47192eeffa4e70
Par Winzell <zell@threerings.net>
2011-10-18 22:44:35 -0500

    Stop worrying about SpotSceneRegistry, and leave inter-peer registration to a higher level.

--- src/main/java/com/threerings/orth/instance/server/InstanceRegistry.java
+++ src/main/java/com/threerings/orth/instance/server/InstanceRegistry.java
@@ -6,30 +6,16 @@ package com.threerings.orth.instance.server;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import com.google.inject.internal.Preconditions;
 
-import com.threerings.crowd.server.BodyLocator;
-
-import com.threerings.whirled.spot.server.SpotSceneRegistry;
-
 import com.threerings.orth.instance.data.Instance;
-import com.threerings.orth.instance.data.InstanceInfo;
-import com.threerings.orth.peer.server.OrthPeerManager;
 
 import static com.threerings.orth.Log.log;
 
 @Singleton
 public class InstanceRegistry
 {
-    @Inject public InstanceRegistry (SpotSceneRegistry reg)
-    {
-        Preconditions.checkState(reg instanceof InstancingSceneRegistry,
-            "Failing: SpotSceneRegistry must be bound to InstancingSceneRegistry.");
-    }
-
     public Instance getInstance (String instanceId)
     {
         return _instances.get(instanceId);
@@ -43,20 +29,8 @@ public class InstanceRegistry
         Preconditions.checkState(!_instances.containsKey(instance.getInstanceId()),
             "Instance '%s' already registered", instance.getInstanceId());
         _instances.put(instance.getInstanceId(), instance);
-
-        // NOTE: This does no locking or coordination between peers. It's purely informative.
-        InstanceInfo info = instance.toInfo();
-        if (_peerman.getOrthNodeObject().instances.contains(info)) {
-            log.warning("InstanceInfo already registered on OrthNodeObject",
-                "instance", instance.getInstanceId());
-        } else {
-            _peerman.getOrthNodeObject().addToInstances(info);
-        }
+        log.debug("Registered local instance", "instance", instance);
     }
 
     protected Map<String, Instance> _instances = Maps.newHashMap();
-
-    @Inject protected Injector _injector;
-    @Inject protected BodyLocator _locator;
-    @Inject protected OrthPeerManager _peerman;
 }
