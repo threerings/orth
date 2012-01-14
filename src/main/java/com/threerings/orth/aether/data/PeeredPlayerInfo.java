4724d821221e65dd6cacb631b021016f570032b0
Par Winzell <zell@threerings.net>
2012-01-14 17:29:28 -0600

    Come to think of it, this never goes to the client.

--- src/main/java/com/threerings/orth/aether/data/PeeredPlayerInfo.java
+++ src/main/java/com/threerings/orth/aether/data/PeeredPlayerInfo.java
@@ -4,11 +4,13 @@
 package com.threerings.orth.aether.data;
 
 import com.threerings.io.SimpleStreamableObject;
+import com.threerings.util.ActionScript;
 
 import com.threerings.orth.aether.data.AetherAuthName;
 import com.threerings.orth.data.PlayerName;
 import com.threerings.orth.data.where.Whereabouts;
 
+@ActionScript(omit=true)
 public class PeeredPlayerInfo extends SimpleStreamableObject
 {
     public AetherAuthName authName;
