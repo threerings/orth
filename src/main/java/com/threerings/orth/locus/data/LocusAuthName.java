3cec4bcf6fd8aff64c0c1ad4647c9d74a66580a7
Pär Winzell <zell@threerings.net>
2012-05-31 15:08:17 -0500

    A long time ago we made all LocusAuthNames equal(). That was perhaps a bit overzealous.
    The purpose was to be able to use a single LocusAuthName.makeKey() to locate any type of Locus connection anywhere, through the peer nodes. But of course it had other, less desirable effects as well, to wit: connecting as RoomAuthName, and the system gladly telling you you've resumed an InterventionName connection. Not good.
    
    So we revert this change and find another way of locating Locus bodies.

--- src/main/java/com/threerings/orth/locus/data/LocusAuthName.java
+++ src/main/java/com/threerings/orth/locus/data/LocusAuthName.java
@@ -6,34 +6,16 @@ package com.threerings.orth.locus.data;
 
 import com.threerings.orth.data.AuthName;
 
-public class LocusAuthName extends AuthName
+/**
+ * LocusAuthName has no current purpose, other than to mark Locus connections.
+ */
+public abstract class LocusAuthName extends AuthName
 {
-    /**
-     * Creates an instance that can be used as a DSet key.
-     */
-    public static LocusAuthName makeKey (int playerId)
-    {
-        return new LocusAuthName("", playerId);
-    }
-
     /**
      * Creates a name for the member with the supplied account name and id.
      */
     public LocusAuthName (String accountName, int id)
     {
         super(accountName, id);
-    }
-
-    @Override public boolean equals (Object other)
-    {
-        // LocusAuthName is different from AuthName in that subclasses can be equal()
-        return other != null && other instanceof LocusAuthName &&
-            (((AuthName)other).getId() == getId());
-    }
-
-    @Override
-    public String getDiscriminator ()
-    {
-        return LocusAuthName.class.getName();
     }
 }
