8543b43c25864ffd561a31f21848fb8406badaa8
Pär Winzell <zell@threerings.net>
2012-06-09 11:33:24 -0500

    Cautionary messages.

--- src/main/java/com/threerings/orth/party/client/PartyRegistryService.java
+++ src/main/java/com/threerings/orth/party/client/PartyRegistryService.java
@@ -16,11 +16,19 @@ public interface PartyRegistryService extends InvocationService<AetherClientObje
 {
     /**
      * Creates a new party with the requester as its leader. Sends a HostedNodelet back.
+     * 
+     * Note that while the listener won't return until the party has been sucessfully created
+     * and hosted on the server, the client still needs to connect and subscribe before any
+     * operations will be successful.
      */
     void createParty (PartyConfig config, ResultListener rl);
 
     /**
      * Joins the given party, which must be open. Sends a HostedNodelet back.
+     *
+     * Note that while the listener won't return until the party has been sucessfully created
+     * and hosted on the server, the client still needs to connect and subscribe before any
+     * operations will be successful.
      */
     void joinParty (int partyId, ResultListener rl);
 }
